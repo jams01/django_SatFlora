96d591f8b816eef0359af342aace990b34e9a763
jams <arley.jams@gmail.com>
2023-07-17 19:39:01 -0500

    cambio los requerimientos

--- django_SatFlora/settings.py
+++ django_SatFlora/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import AutoConfig
+
+from pathlib import Path
+
+config = AutoConfig(search_path='./django_SatFlora/')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
